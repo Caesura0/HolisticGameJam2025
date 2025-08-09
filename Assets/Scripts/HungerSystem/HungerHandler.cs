c752286822fcf872ffc2d11641c67375f1c4fed0
Ferroismo <ferroismo98@gmail.com>
2025-08-09 19:44:35 +0200

    Hunger Display
    -Added display for hunger system
    -Display icons are placeholders, need to be replaced when possible.

--- Assets/Scripts/HungerSystem/HungerHandler.cs
+++ Assets/Scripts/HungerSystem/HungerHandler.cs
@@ -1,6 +1,9 @@
+using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HungerHandler : MonoBehaviour
 {
@@ -36,6 +39,7 @@ public class HungerHandler : MonoBehaviour
 
         currentHunger = hungerLimit;
         ResetTimer();
+        InitializeDisplay();
     }
 
     private void Update()
@@ -46,10 +50,42 @@ public class HungerHandler : MonoBehaviour
         {
             ResetTimer();
             currentHunger--;
+            UpdateDisplay();
             OnHungerUpdate?.Invoke(currentHunger);
         }
         timerUI.text = TimeSpan.FromSeconds(remainingResetTime).ToString(@"mm\:ss");
     }
 
     private void ResetTimer() => remainingResetTime = hungerResetTime;
+
+    private Image[] displayIcons;
+    private void InitializeDisplay()
+    {
+        List<Image> icons = new List<Image>();
+        for(int i = 0; i < hungerLimit; i++)
+        {
+            Image display = 
+                Instantiate(hungerIconPrefab, hungerIconHolder.transform).GetComponent<Image>();
+            icons.Add(display);
+        }
+        displayIcons = icons.ToArray();
+
+        UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        for(int i = 0; i < hungerLimit; i++)
+        {
+            if (i > currentHunger - 1)
+                displayIcons[i].gameObject.SetActive(false);
+            else
+                displayIcons[i].gameObject.SetActive(true);
+
+            if (currentHunger <= hungerRagePoint)
+                displayIcons[i].color = Color.red;
+            else
+                displayIcons[i].color = Color.white;
+        }
+    }
 }
