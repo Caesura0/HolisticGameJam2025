1bf509d3315b02e6eb7b5e156656deacfc16a30b
HIGH-COMMAND\jskar <jskarupa1@gmail.com>
2025-08-15 21:54:51 -0400

    Created more prefabs, adding pickupable logic.

--- Assets/Scripts/PickUpSystem/PickableItem.cs
+++ Assets/Scripts/PickUpSystem/PickableItem.cs
@@ -7,15 +7,21 @@ public class PickableItem: MonoBehaviour
     Rigidbody2D rb;
     Transform holder;
     bool beingHeld = false;
+    Collider2D collision;
+    const float stopThreshold = 0.1f;
+    bool wasMoving = false;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        collision = GetComponent<Collider2D>();
     }
     
     private void Update()
     {
         if(beingHeld)
             transform.position = holder.position;
+        CheckIfStopped();
     }
     
     public void PickUp(Transform assignedHolder)
@@ -23,12 +29,15 @@ public class PickableItem: MonoBehaviour
         rb.bodyType = RigidbodyType2D.Kinematic;
         holder = assignedHolder;
         beingHeld = true;
+        collision.enabled = false; // Disable collider while being held
+
     } 
 
     public void Throw(Vector3 velocity)
     {
         Release();
         rb.AddForce (velocity, ForceMode2D.Impulse);
+        //activate trigger collider
     }
 
     public void Release()
@@ -36,5 +45,30 @@ public class PickableItem: MonoBehaviour
         rb.bodyType = RigidbodyType2D.Dynamic;
         holder = null;
         beingHeld = false;
+    }
+
+
+
+    void CheckIfStopped()
+    {
+        // Only check if Rigidbody2D is dynamic
+        if (rb.bodyType == RigidbodyType2D.Dynamic)
+        {
+            if (rb.linearVelocity.magnitude > stopThreshold)
+            {
+                wasMoving = true;
+            }
+            else if (wasMoving)
+            {
+                wasMoving = false;
+                OnStopMoving();
+            }
+        }
+    }
+
+    void OnStopMoving()
+    {
+
+        collision.enabled = true; 
     }
 }
