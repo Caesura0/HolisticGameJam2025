80bb86f75af15605b6c098b9156b3aa1653560bd
Ferroismo <ferroismo98@gmail.com>
2025-08-17 21:06:39 +0200

    Death Animation & Eat Alert
    -Added death animation
    -Added visual notification on eat

--- Assets/Scripts/Misc/Notification/NotificationHandler.cs
+++ Assets/Scripts/Misc/Notification/NotificationHandler.cs
@@ -3,14 +3,17 @@ using UnityEngine;
 public enum NotificationType
 {
     Alert,
-    Hunger
+    Hunger,
+    Eat
 }
 public class NotificationHandler : MonoBehaviour
 {
     private const string AlertString = "Alert";
     private const string HungerString = "Hunger";
+    private const string EatString = "Eat";
     private int AlertTriggerId = Animator.StringToHash(AlertString);
     private int HungerTriggerId = Animator.StringToHash(HungerString);
+    private int EatTriggerId = Animator.StringToHash(EatString);
 
     [SerializeField] private Animator animator;
     private bool initialized;
@@ -34,6 +37,9 @@ public class NotificationHandler : MonoBehaviour
                 break;
             case NotificationType.Hunger:
                 chosenId = HungerTriggerId;
+                break;
+            case NotificationType.Eat:
+                chosenId = EatTriggerId;
                 break;
         }
 
